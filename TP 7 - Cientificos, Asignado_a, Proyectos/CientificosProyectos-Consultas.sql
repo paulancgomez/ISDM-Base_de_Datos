b8817236044addcf5a44b14c2b1233429f14e6d8
paulancgomez <paulancgomez@gmail.com>
2022-07-12 05:54:54 -0300

    Mejoras TP

--- TP 7 - Cientificos, Asignado_a, Proyectos/CientificosProyectos-Consultas.sql
+++ TP 7 - Cientificos, Asignado_a, Proyectos/CientificosProyectos-Consultas.sql
@@ -1,41 +1,47 @@
 -- CONSULTAS
-use cientificosproyectos;
+
+USE cientificosproyectos;
 
 -- 1.- Listar todos los proyectos ordenados descendentemente por nombre
 SELECT nombre FROM proyecto ORDER BY nombre DESC;
 
 -- 2.- Listar todos los científicos que en su nombre contengan la frase ‘ANA’
-SELECT nomApels FROM cientificos WHERE nomApels LIKE '%ANA%';
+SELECT nomApels FROM cientificos WHERE nomApels LIKE'%ANA%';
 
 -- 3.- Mostrar todos los científicos que trabajan en el proyecto REDES INFORMATICAS. Realice la consulta con subconsultas, producto cartesiano e inner join
 SELECT nomApels FROM cientificos WHERE dni IN (SELECT cientifico FROM asignado_a WHERE proyecto IN (SELECT id FROM proyecto WHERE nombre = 'REDES INFORMATICAS')); -- SUBCONSULTAS
-SELECT nomApels FROM cientificos INNER JOIN asignado_a ON cientifico = dni INNER JOIN proyecto ON proyecto = id AND nombre = 'REDES INFORMATICAS'; -- INNER JOIN
+SELECT nomApels, nombre FROM cientificos, asignado_a, proyecto WHERE cientifico = dni AND proyecto = id AND nombre = 'REDES INFORMATICAS'; -- PRODUCTO CARTESIANO
+SELECT nomApels, nombre FROM cientificos INNER JOIN asignado_a ON cientifico = dni INNER JOIN proyecto ON proyecto = id AND nombre = 'REDES INFORMATICAS'; -- INNER JOIN
 
 -- 4.- Mostrar todos los proyectos en los que trabajan el científico ‘CRISTIAN RUEDA’. Realice la consulta con subconsultas, producto cartesiano e inner join
 SELECT nombre FROM proyecto WHERE id IN (SELECT proyecto FROM asignado_a WHERE cientifico IN (SELECT dni FROM cientificos WHERE nomApels = 'CRISTIAN RUEDAS')); -- SUBCONSULTAS
-SELECT nombre FROM proyecto, asignado_a, cientificos WHERE id = proyecto AND dni = cientifico AND nomApels = 'CRISTIAN RUEDAS'; -- PRODUCTO CARTESIANO
-SELECT nombre FROM proyecto INNER JOIN asignado_a ON id = proyecto INNER JOIN cientificos ON dni = cientifico AND nomApels = 'CRISTIAN RUEDAS';
+SELECT nomApels, nombre FROM proyecto, asignado_a, cientificos WHERE id = proyecto AND dni = cientifico AND nomApels = 'CRISTIAN RUEDAS'; -- PRODUCTO CARTESIANO
+SELECT nomApels, nombre FROM proyecto INNER JOIN asignado_a ON id = proyecto INNER JOIN cientificos ON dni = cientifico AND nomApels = 'CRISTIAN RUEDAS';
 
 -- 5.- Mostrar en cuantos proyectos trabaja ‘CRISTIAN RUEDA’.
-SELECT COUNT(proyecto) FROM asignado_a WHERE cientifico IN (SELECT dni FROM cientificos WHERE nomApels = 'CRISTIAN RUEDAS'); 
+SELECT COUNT(proyecto) AS CANT_PROYECTOS FROM asignado_a WHERE cientifico IN (SELECT dni FROM cientificos WHERE nomApels = 'CRISTIAN RUEDAS'); -- SUBCONSULTAS
+SELECT nomApels, COUNT(proyecto) AS CANT_PROYECTOS FROM cientificos, asignado_a WHERE dni = cientifico AND nomApels = 'CRISTIAN RUEDAS'; -- PRODUCTO CARTESIANO
+SELECT nomApels, COUNT(proyecto) AS CANT_PROYECTOS FROM cientificos INNER JOIN asignado_a ON dni = cientifico WHERE nomApels = 'CRISTIAN RUEDAS'; -- INNER JOIN
 
 -- 6.- Obtener un listado de los científicos conjuntamente con los datos del proyecto en el cual trabaja. Realice la consulta con producto cartesiano e inner join
-SELECT * FROM cientificos, proyecto, asignado_a WHERE dni=cientifico AND proyecto=id; -- PRODUCTO CARTESIANO
-SELECT * FROM cientificos INNER JOIN asignado_a ON dni=cientifico INNER JOIN proyecto ON proyecto=id;
+SELECT dni, nomApels, nombre AS proyecto, horas FROM cientificos, proyecto, asignado_a WHERE dni = cientifico AND proyecto = id; -- PRODUCTO CARTESIANO
+SELECT dni, nomApels, nombre AS proyecto, horas FROM cientificos INNER JOIN asignado_a ON dni = cientifico INNER JOIN proyecto ON proyecto = id; -- INNER JOIN
 
 -- 7.-Obtener el numero de proyectos al que está asignado cada científico.
-SELECT cientifico, COUNT(proyecto) FROM asignado_a GROUP BY cientifico; -- SUBCONSULTAS
-SELECT nomApels AS NOMBRE_CIENTIFICO, COUNT(proyecto) AS CANT_PROYECTOS FROM cientificos INNER JOIN asignado_a ON dni=cientifico GROUP BY cientifico; -- INNER JOIN
+SELECT nomApels AS CIENTIFICO, COUNT(proyecto) AS CANT_PROYECTOS FROM cientificos, asignado_a WHERE dni = cientifico GROUP BY cientifico; -- PRODUCTO CARTESIANO
+SELECT nomApels AS CIENTIFICO, COUNT(proyecto) AS CANT_PROYECTOS FROM cientificos INNER JOIN asignado_a ON dni = cientifico GROUP BY cientifico; -- INNER JOIN
 
 -- 8.- Obtener el número de científicos al que está asignado cada proyecto.
-SELECT COUNT(cientifico) FROM asignado_a GROUP BY proyecto; -- SUBCONSULTAS
-SELECT nombre AS NOMBRE_PROYECTO, COUNT(cientifico) AS CANT_CIENTIFICOS FROM asignado_a INNER JOIN proyecto ON proyecto=id GROUP BY proyecto; -- INNER JOIN 
+SELECT nombre AS PROYECTO, COUNT(cientifico) AS CANT_CIENTIFICOS FROM asignado_a, proyecto WHERE proyecto = id GROUP BY proyecto; -- PRODUCTO CARTESIANO 
+SELECT nombre AS PROYECTO, COUNT(cientifico) AS CANT_CIENTIFICOS FROM asignado_a INNER JOIN proyecto ON proyecto = id GROUP BY proyecto; -- INNER JOIN 
 
 -- 9.- Obtener el numero de horas de dedicación de cada científico.
-SELECT cientifico, SUM(horas) FROM proyecto INNER JOIN asignado_A ON id=proyecto GROUP BY cientifico;
+SELECT nomApels AS CIENTIFICO, SUM(horas) AS TOTAL_HORAS FROM cientificos, asignado_a, proyecto WHERE dni = cientifico AND proyecto = id GROUP BY cientifico; -- PRODUCTO CARTESIANO
+SELECT nomApels AS CIENTIFICO, SUM(horas) AS TOTAL_HORAS FROM cientificos INNER JOIN asignado_a ON dni = cientifico INNER JOIN proyecto ON proyecto = id GROUP BY cientifico; -- INNER JOIN
 
 -- 10.- Obtener el DNI y el nombre de los científicos que se dedican a mas de un proyecto y cuya dedicación media (promedio) a cada proyecto sea superior a las 80 horas.
-SELECT dni, nomApels FROM cientificos INNER JOIN asignado_a ON cientifico = dni INNER JOIN proyecto ON proyecto=id GROUP BY dni HAVING COUNT(cientifico) > 1 AND AVG(horas) > 80;
+SELECT dni, nomApels FROM cientificos, asignado_a, proyecto WHERE dni = cientifico AND proyecto = id GROUP BY dni HAVING COUNT(cientifico) > 1 AND AVG(horas) > 80; -- PRODUCTO CARTESIANO
+SELECT dni, nomApels FROM cientificos INNER JOIN asignado_a ON dni = cientifico INNER JOIN proyecto ON proyecto = id GROUP BY dni HAVING COUNT(cientifico) > 1 AND AVG(horas) > 80; -- INNER JOIN
 
 -- 11.- Mostrar todos los proyectos que tengan mas horas que el promedio de horas de todos los proyectos
-SELECT nombre FROM proyecto WHERE horas > (SELECT AVG(horas) FROM proyecto);
+SELECT nombre AS PROYECTO FROM proyecto WHERE horas > (SELECT AVG(horas) FROM proyecto);
--- TP 7 - Cientificos, Asignado_a, Proyectos/CientificosProyectos.sql
+++ TP 7 - Cientificos, Asignado_a, Proyectos/CientificosProyectos.sql
@@ -1,43 +1,43 @@
 -- MUESTRA TODAS LAS BASES DE DATOS QUE TENGO
 SHOW DATABASES;
 
--- Si existe la BD 'clase20' la borra
+-- Si existe la BD 'CientificosProyectos' la borra
 DROP DATABASE IF EXISTS CientificosProyectos;
 
 -- SE CREA LA BASE DE DATOS
 CREATE DATABASE CientificosProyectos;
 
--- USA LA BASE DE DATOS Pruebas
+-- USA LA BASE DE DATOS CientificosProyectos
 USE CientificosProyectos;
 
--- SI LA TABLA vendedores EXISTE, LA BORRA
+-- SI LA TABLA cientificos EXISTE, LA BORRA
 DROP TABLE IF EXISTS cientificos;
 
--- CREA LA TABLA vendedores
+-- CREA LA TABLA cientificos
 CREATE TABLE cientificos(
 	dni VARCHAR(8),
     nomApels NVARCHAR(255),
     PRIMARY KEY (dni)
 );
 
--- SI LA TABLA clientes EXISTE, LA BORRA
+-- SI LA TABLA proyecto EXISTE, LA BORRA
 DROP TABLE IF EXISTS proyecto;
 
--- CREA LA TABLA clientes
+-- CREA LA TABLA proyecto
 CREATE TABLE proyecto(
-	id INT AUTO_INCREMENT,
+	id CHAR(4),
     nombre NVARCHAR(255),
     horas INT,
     PRIMARY KEY (id)
 );
 
--- SI LA TABLA pedidos EXISTE, LA BORRA
-DROP TABLE IF EXISTS pedidos;
+-- SI LA TABLA asignado_a EXISTE, LA BORRA
+DROP TABLE IF EXISTS asignado_a;
 
--- CREA LA TABLA clientes
+-- CREA LA TABLA asignado_a
 CREATE TABLE asignado_a(
 	cientifico VARCHAR(8),
-    proyecto INT,
+    proyecto CHAR(4),
     PRIMARY KEY (cientifico, proyecto),
     FOREIGN KEY (cientifico) REFERENCES cientificos(dni),
     FOREIGN KEY (proyecto) REFERENCES proyecto(id)
@@ -46,29 +46,29 @@ CREATE TABLE asignado_a(
 -- MUESTRA LAS TABLAS QUE TENGO
 SHOW TABLES;
 
--- INSERTAMOS LOS VALORES A LA TABLA vendedores
+-- INSERTAMOS LOS VALORES A LA TABLA cientificos
 INSERT INTO cientificos(dni, nomApels)
 VALUES(21000111, "ANA PEREZ"),
 	  (22000111, "EMA WATSON"),
       (23000111, "PEDRO PICAPIEDRAS"),
       (24000111, "CRISTIAN RUEDAS");
 
--- BORRAR LA TABLA vendedores
+-- BORRAR LA TABLA proyecto
 DROP TABLE proyecto;      
 
--- INSERTAMOS LOS VALORES A LA TABLA clientes
-INSERT INTO proyecto(nombre, horas)
-VALUES("REDES INFORMATICAS", 50),
-	  ("A", 60),
-      ("B", 70),
-      ("C", 80),
-      ("D", 90),
-      ("E", 100);
+-- INSERTAMOS LOS VALORES A LA TABLA proyecto
+INSERT INTO proyecto(id, nombre, horas)
+VALUES(1, "REDES INFORMATICAS", 50),
+	  (2, "A", 60),
+      (3, "B", 70),
+      (4, "C", 80),
+      (5, "D", 90),
+      (6, "E", 100);
 
--- BORRAR LA TABLA clientes
+-- BORRAR LA TABLA asignado_a
 DROP TABLE asignado_a;
 
--- INSERTAMOS LOS VALORES A LA TABLA pedidos
+-- INSERTAMOS LOS VALORES A LA TABLA asignado_a
 INSERT INTO asignado_a(cientifico, proyecto)
 VALUES(21000111, 1),
 	  (22000111, 2),
@@ -79,5 +79,5 @@ VALUES(21000111, 1),
       (23000111, 1),
       (24000111, 2);
 
--- BORRAR LA TABLA pedidos
-DROP TABLE pedidos;
+-- BORRAR LA TABLA asignado_a
+DROP TABLE asignado_a;
